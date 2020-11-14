3409343d5caf1e501bde5e2b5d5c7ef1a1a75aa4
Mohammed Al-Asfar <elasfarc@gmail.com>
2020-11-14 19:11:30 +0200

    Add User model with validations

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    validates :_name, presence: true, uniqueness: true, length: {minimum: 5, maximum: 12}
+    validates :email, presence: true
+    validates :password, presence: true, length: {minimum: 8, maximum: 64}
+end
